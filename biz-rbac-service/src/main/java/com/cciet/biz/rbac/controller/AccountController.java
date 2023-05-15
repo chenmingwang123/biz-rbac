3d5da2c5a7c8c3b1a5b3d31ba417d60b5cd4e2dd
HuangHui <sky.os@outlook.com>
2023-05-15 14:05:05 +0800

    Account

---
+++ biz-rbac-service/src/main/java/com/cciet/biz/rbac/controller/AccountController.java
@@ -0,0 +1,18 @@
+package com.cciet.biz.rbac.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 用户账号 前端控制器
+ * </p>
+ *
+ * @author huanghui
+ * @since 2023/05/15 09:09
+ */
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+}
